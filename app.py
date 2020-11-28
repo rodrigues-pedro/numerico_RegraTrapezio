d919469516a8c1549907d9837cd863ac48ecdba4
rodrigues-pedro <rodpedromanuel@gmail.com>
2020-11-28 18:19:21 -0300

    asd

--- app.py
+++ app.py
@@ -12,24 +12,25 @@ class Func(FlaskForm):
     function = StringField('Função à ser Integrada', validators=[DataRequired()])
     x_o = DecimalField('Valor inicial do Intervalo', validators=[DataRequired()])
     x_f = DecimalField('Valor final do Intervalo', validators=[DataRequired()])
-    erro_n = DecimalField('Valor inicial do Intervalo', validators=[DataRequired()])
+    erro_n = DecimalField('Erro ou Número de Trapézios', validators=[DataRequired()])
     submit = SubmitField('Submit', validators=[DataRequired()])
 
 @app.route('/', methods=['GET', 'POST'])
 def newPost():
-    form = NewPost()
+    form = Func()
+    '''
     post = {}
     i = 1
     if form.validate_on_submit():
         i = 0
-        flash('New post from {}, titled {}'.format(form.author.data, form.title.data))
         postTitle = form.title.data
         postContent = form.content.data
         postAuthor = form.author.data
         post = {'title': form.title.data,'content': form.content.data,'author': form.author.data}
         config.add_post(postTitle, postContent, postAuthor)
         return render_template('newPost.html', form=form, post=post, i=i)
-    return render_template('newPost.html', form=form, post=post, i=i)
+    '''
+    return render_template('index.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
