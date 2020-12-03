753665b70a2dddb377860de897088eb9dcbd4507
Murilo Rodrigues <murilo2759@gmail.com>
2020-12-03 18:35:22 -0300

    js e html -- modal

--- static/js/intervalo.js
+++ static/js/intervalo.js
@@ -1,11 +1,19 @@
-const xInicial = document.querySelector('#x_0');
+const xInicial = document.querySelector('#x_o');
 const xFinal = document.querySelector('#x_f');
 const calcular = document.querySelector('#btn-submit');
 
 calcular.addEventListener('click',function(){
     if(xInicial.value > xFinal.value){
         alert('O valor inicial do intervalo não pode ser maior que o final. Por favor, reescreva o intervalo');
-        xInicial.textContent = '';
-        xFinal.textContent = '';
+        xInicial.value = '';
+        xFinal.value = '';
+        document.reload(false);
+    }
+    console.log(xInicial.textContent)
+});
+
+$("#form").submit(function() {
+    if($("#x_o").val()== null || $("#x_f").val() =="" || $("#function").val() == "" || $("#erro_n").val() == ""){
+        return false;
     }
 });
